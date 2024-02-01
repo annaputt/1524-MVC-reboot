eea6f4920ec1747c2f306b04196e926f3f687a27
Anna Putt anna.putt@lewagon.orgexec zs <anna.putt@lewagon.org>
2024-02-01 16:28:43 +0100

    MVC reboot

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,57 +6,86 @@ class BooksController
   end
 
   def list
-    # Fetch ALL the books from Active Record
-    # Display all books in View
+    # Fetch ALL the books from Active Record. Create a variable with 'books =...' because you will use the data later
+    books = Book.all
+    # Display all books in View. @view connects to the view tab. Parenthesies passes the argument.
+    @view.display(books)
   end
 
   def add
     # Ask for title => View
+    title = @view.ask_user_for("title")
     # Ask for price
+    price = @view.ask_user_for("price").to_i
     # Ask for author
+    author = @view.ask_user_for("author")
     # Ask for category
+    category = @view.ask_user_for("category")
 
     # Active Record
+    book = Book.new(title: title, price: price)
+    book.save
     # Create a new book
     # Save a new book
   end
 
   def edit
     # Fetch ALL the books from Active Record
+    books = Book.all
     # Display all books
-
+    @view.display(books)
     # Ask for which book by ID
-
+    id = @view.ask_user_for("ID").to_i
     # Find the book
-
+    book = Book.find(id)
     # Ask for title => View
+    new_title = @view.ask_user_for("title")
     # Ask for price
+    new_price = @view.ask_user_for("price").to_i
     # Ask for author
+    new_author = @view.ask_user_for("author")
     # Ask for category
+    new_category = @view.ask_user_for("category")
 
     # Edit the data
-    # Save the new witrh new data
+    book.title = new_title
+    book.price = new_price
+    book.author = new_author
+    book.category = new_category
+    # Save the new with new data
+    book.save
 
   end
 
   def delete
     # Fetch ALL the books from Active Record
     # Display all books
-
+    books = Book.all
+    @view.display(books)
     # Ask for which book by ID
-
+    id = @view.ask_user_for("ID").to_i
     # Find the book
-
-    # Detroy the book
+    book = Book.find(id)
+    # Detroy the book - doesn't need to be in a variable, so doesn't need to be a book = book.destroy
+    book.destroy
   end
 
   def mark_as_rent
     # Fetch ALL the books from Active Record
     # Display all books
-
+    books = Book.all
+    @view.display(books)
     # Ask for which book by ID
+    id = @view.ask_user_for("ID").to_i
     # Find the book
+    book = Book.find(id)
     # Mark as rent
+    if book.rent # => true, i.e. the book has been rented out, so returns false as it is not available
+      book.rent = false
+    else
+      book.rent = true
+    end
     # Save
+    book.save
   end
 end
--- app/views/books_view.rb
+++ app/views/books_view.rb
@@ -1,2 +1,17 @@
 class BooksView
+  def display(books)
+    books.each do |book|
+      status = book.rent ? "Unavailable" : "Available"
+      # book.rent = !book.rent # => same as line above just shorter
+
+      puts "\nID:#{book.id} - TITLE: #{book.title}"
+
+      puts "#{status} #{book.id} = #{book.title}"
+    end
+  end
+
+  def ask_user_for(thing)
+    puts "what is #{thing}?"
+    gets.chomp
+  end
 end
